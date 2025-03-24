2fc15dff0bf135afe60ded1bf35c7aa04595b1a9
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-03-24 10:56:58 -0300

    Remove código comentado

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddScoped<PasswordService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
